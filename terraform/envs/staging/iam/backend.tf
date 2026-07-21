10973aac326535d2abff90ce0559983927937e35
Herbert Tamayo <hftamayo@gmail.com>
2026-07-21 13:08:45 -0600

    removing deprecated staging/iam

--- terraform/envs/staging/iam/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "absencesbo-terraform-state"
-    key            = "iam/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "absencesbo-terraform-locks"
-  }
-}
